e8c2d836395d920eb9bc67fa97de45ecfa16056d
Marcus Bragion <viniciusbragion@outlook.pt>
2024-07-27 13:55:53 -0300

    feat: adicionado exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,12 @@
+from typing import Dict, Any
+
+livro: Dict[str, Any] = {   
+    'titulo': 'Orgulho e Preconceito',
+    'autor': 'Jane Austen',
+    'paginas': 432,
+    'disponivel': True,
+    'distruibor': 'Saraiva'
+}
+
+print(livro)
+
